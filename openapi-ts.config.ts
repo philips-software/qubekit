882f983bdb92974909c0efb5338e94ae9292097a
Scott Guymer <scott.guymer@philips.com>
2025-05-09 09:49:24 +0200

    feat!: Initial Release
    First release of qubekit

---
+++ openapi-ts.config.ts
@@ -0,0 +1,34 @@
+import {
+  createClient,
+  defaultPlugins,
+  defineConfig,
+} from '@hey-api/openapi-ts';
+
+export default {
+  experimentalParser: true,
+  input: 'src/schema/sonarqube-all.yaml',
+  output: {
+    format: 'prettier',
+    lint: 'eslint',
+    path: `src/generated`,
+  },
+  plugins: [
+    ...defaultPlugins,
+    { name: '@hey-api/client-fetch', baseUrl: '' },
+    '@hey-api/schemas',
+    {
+      name: '@hey-api/transformers',
+      dates: true,
+    },
+    {
+      name: '@hey-api/typescript',
+      enums: 'javascript',
+      readOnlyWriteOnlyBehavior: 'off',
+    },
+    {
+      name: '@hey-api/sdk',
+      asClass: true,
+      transformer: true,
+    },
+  ],
+};
