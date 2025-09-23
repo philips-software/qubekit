f70d7e90e3dd39529958a5e6b3d0e112e9b4bcb5
Scott Guymer <scott.guymer@philips.com>
2025-09-23 09:16:32 +0200

    Ensure the type is exported

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { createQubekit } from './qubekit';
+export { createQubekit, type QubekitOptions } from './qubekit';
 export type { Qubekit } from './qubekitClient';
 export * from './generated/sdk.gen';
 export type * from './generated/types.gen';
